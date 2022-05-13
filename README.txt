39b7e21db69fd8fd4767848d086b379ea7d01c91
Marc A. Taurasi <altairre.atsu@gmail.com>
2022-05-12 22:39:57 -0600

    Initial commit

---
+++ README.txt
@@ -0,0 +1,25 @@
+Tape Measure 
+by Quill Inkwell
+Version 0.1
+
+Installation Instructions:
+===============================================
+The following zip is packaged to allow for easy installation of the Mod into your game.
+Simply extract the contents of the provided zip file into your CyubeVR Mods Folder.
+"cyubeVR/cyubeVR/Mods"
+If you are updating the mod allow it to overwrite any existing mod files it encounters.
+
+Usage Instructions:
+===============================================
+Once installed craft the Tape Measure Start and Tape Measure end blocks found in the "Tape Measure" category.
+To use simply place the Tape Measure start block wherever you would like to begin your measurement.
+Next place the Tape Measure end block wherever you would like to end your measurement.
+These blocks do not need to be in line and it is even possible to measure a 3d volume.
+To measure the distance simply tap either block with a stick.
+
+Uninstallation Instructions:
+===============================================
+Simply delete the TapeMeasure__V1 mod from the "APIMods" folder.
+To remove the blocks delete the following folders from your "Blocks" folder:
+quillTapeMeasure
+quillTapeMeasureEnd
---
+++ Source/ProjectFiles/Source/Mod.cpp
@@ -0,0 +1,73 @@
+#include "GameAPI.h"
+
+/************************************************************
+	Config Variables (Set these to whatever you need. They are automatically read by the game.)
+*************************************************************/
+float TickRate = 1;
+
+const int Tape_Measure_Start_Block = 3040;
+const int Tape_Measure_End_Block = 3041;
+
+UniqueID ThisModUniqueIDs[] = { Tape_Measure_End_Block, Tape_Measure_Start_Block };
+
+CoordinateInBlocks startPosition;
+CoordinateInBlocks endPosition;
+
+/************************************************************* 
+//	Functions (Run automatically by the game, you can put any code you want into them)
+*************************************************************/
+
+void Event_BlockPlaced(CoordinateInBlocks At, UniqueID CustomBlockID, bool Moved)
+{
+	if      (CustomBlockID == Tape_Measure_End_Block) {
+		endPosition = At;
+	}
+	else if (CustomBlockID == Tape_Measure_Start_Block) {
+		startPosition = At;
+	}
+}
+
+void Event_BlockDestroyed(CoordinateInBlocks At, UniqueID CustomBlockID, bool Moved)
+{
+
+}
+
+void Event_BlockHitByTool(CoordinateInBlocks At, UniqueID CustomBlockID, wString ToolName)
+{
+	if (ToolName == L"T_Stick" && CustomBlockID == Tape_Measure_End_Block || CustomBlockID == Tape_Measure_Start_Block) {
+		int64_t distanceX = endPosition.X - startPosition.X;
+		int64_t distanceY = endPosition.Y - startPosition.Y;
+		int16_t distanceZ = endPosition.Z - startPosition.Z;
+		int64_t totalDistance = sqrt((distanceX * distanceX) + (distanceY * distanceY) + (distanceZ * distanceZ));
+
+		std::wstring message = L"Distance X: " + std::to_wstring(abs(distanceX)) + std::wstring(L"\n")
+							 + L"Distance Y: " + std::to_wstring(abs(distanceY)) + L"\n"
+							 + L"Distance Z: " + std::to_wstring(abs(distanceZ)) + L"\n"
+							 + L"Distance to point: " + std::to_wstring(abs(totalDistance));
+		SpawnHintText(At+CoordinateInCentimeters(0,0,75), message, 3, 2);
+	}
+}
+
+void Event_Tick()
+{}
+
+void Event_OnLoad()
+{}
+
+void Event_OnExit()
+{}
+
+/*******************************************************
+
+	Advanced functions
+
+*******************************************************/
+
+void Event_AnyBlockPlaced(CoordinateInBlocks At, BlockInfo Type, bool Moved)
+{}
+
+void Event_AnyBlockDestroyed(CoordinateInBlocks At, BlockInfo Type, bool Moved)
+{}
+
+void Event_AnyBlockHitByTool(CoordinateInBlocks At, BlockInfo Type, wString ToolName)
+{}
